46001b58b5e11d12f8839a33ece554ca59d1f0c1
VeryverySoHappy <frontEnd0815@gmail.com>
2026-06-17 17:55:09 +0900

    [atoms] Icon

---
+++ src/shared/assets/images/icon/IconArrowRight.tsx
@@ -0,0 +1,12 @@
+const IconArrowRight = () => (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <mask id="mask0_13218_61952" style={{ maskType: 'alpha' }} maskUnits="userSpaceOnUse" x="7" y="4" width="7" height="12">
+      <path d="M7.91602 15L12.916 10L7.91602 5" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+    </mask>
+    <g mask="url(#mask0_13218_61952)">
+      <rect x="20" width="20" height="20" transform="rotate(90 20 0)" fill="black"/>
+    </g>
+  </svg>
+);
+
+export default IconArrowRight;
